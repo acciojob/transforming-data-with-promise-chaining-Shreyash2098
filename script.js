7b827bbc91b2a331076cea2cd0b466be8a080563
Shreyash2098 <143548443+Shreyash2098@users.noreply.github.com>
2023-11-30 12:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+// script.js
+
+const outputDiv = document.getElementById("output");
+
+function manipulateArray(array) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(array);
+    }, 3000);
+  });
+}
+
+function filterOddNumbers(arr) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const filteredArray = arr.filter((num) => num % 2 === 0);
+      resolve(filteredArray);
+    }, 1000);
+  });
+}
+
+function multiplyEvenNumbers(arr) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const multipliedArray = arr.map((num) => (num % 2 === 0 ? num * 2 : num));
+      resolve(multipliedArray);
+    }, 2000);
+  });
+}
+
+const inputArray = [1, 2, 3, 4];
+
+manipulateArray(inputArray)
+  .then(filterOddNumbers)
+  .then(multiplyEvenNumbers)
+  .then((result) => {
+    outputDiv.textContent = JSON.stringify(result);
+  });
